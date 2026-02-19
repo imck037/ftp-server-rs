594a8a2a39536eb142a5e2a1e6b42e0d7ce8373e
imck037 <chandradhar.2003@gmail.com>
2026-02-19 19:59:20 +0530

    simple server-client connection model is done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    stream.write(b"The server is ready...").unwrap();
+    let mut buffer = [0; 1024];
+    let message_lenth = stream.read(&mut buffer).unwrap();
+    let response = String::from_utf8_lossy(&buffer[..message_lenth]);
+    println!("{}", response);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:2020").expect("Failed to bind...");
+    println!("The ftp server is running on port 2020");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(s) => {
+                println!("Client connected...");
+                thread::spawn(|| {
+                    handle_client(s);
+                });
+            }
+            Err(e) => {
+                println!("Failed to connect to the client...{}", e);
+            }
+        }
+    }
 }
