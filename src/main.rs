10cd77577514221339f29d6e291b397edb2437f8
imck037 <chandradhar.2003@gmail.com>
2026-02-21 19:56:22 +0530

    retr and quit function is added...

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::fs;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::io::Read;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -18,28 +17,29 @@ fn handle_client(mut stream: TcpStream) {
             break;
         }
         let command = command.trim();
-        println!("Command: {command}");
 
         let parts: Vec<&str> = command.split_whitespace().collect();
         if parts.is_empty() {
             continue;
         }
 
-        match parts[0].to_lowercase().as_str() {
-            "user" => {
+        println!("Command: {command}");
+
+        match parts[0].to_uppercase().as_str() {
+            "USER" => {
                 stream
                     .write_all(b"Username is correct. Enter password for authentication.\r\n")
                     .unwrap();
             }
-            "pass" => {
+            "PASS" => {
                 stream.write_all(b"230 Login Successfull.\r\n").unwrap();
             }
-            "pwd" => {
+            "PWD" => {
                 let dir = env::current_dir().unwrap();
                 let dir_response = format!("257 \"{}\"\r\n", dir.display());
                 stream.write_all(dir_response.as_bytes()).unwrap();
             }
-            "list" => {
+            "LIST" => {
                 stream
                     .write_all(b"150 Listing directory and files..")
                     .unwrap();
@@ -52,6 +52,28 @@ fn handle_client(mut stream: TcpStream) {
                     let dirs_response_line = format!("{}", file_name);
                     stream.write_all(dirs_response_line.as_bytes()).unwrap();
                 }
+            }
+            "RETR" => {
+                if parts.len() < 2 {
+                    stream.write_all(b"File is not specified...\r\n").unwrap();
+                    continue;
+                }
+                let filename = parts[1];
+
+                match fs::read(filename) {
+                    Ok(data) => {
+                        stream.write_all(b"150 opening file\r\n").unwrap();
+                        stream.write_all(&data).unwrap();
+                        stream.write_all(b"\r\n226 transfer complete\r\n").unwrap();
+                    }
+                    Err(_) => {
+                        stream.write_all(b"550 file not founc...\r\n").unwrap();
+                    }
+                }
+            }
+            "QUIT" => {
+                stream.write_all(b"leaving the ftp server....\r\n").unwrap();
+                break;
             }
             _ => {
                 stream.write_all(b"Command is not specified..").unwrap();
